26035b965a025673314d9e9690463670bd757011
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-09-24 18:20:51 +0100

    H1

--- homework.py
+++ homework.py
@@ -26,10 +26,11 @@ import pandas as pd
 data = pd.DataFrame(
     {'y1': [0.24, 0.06, 0.04, 0.36, 0.32, 0.68, 0.90, 0.76, 0.46, 0.62, 0.44, 0.52],
     'y2': [1, 2, 0, 0, 0, 2, 0, 2, 1, 0, 1, 0],
-    'y3': [1, 0, 0, 2, 0, 2, 1, 2, 1, 0, 2, 2],,    'y4': [0, 0, 0, 1, 2, 1, 2, 0, 1, 1, 2, 0],i    'y_out': ['A', 'B', 'B', 'C', 'C', 'A', 'A', 'A', 'B', 'B', 'C', 'C']u
-e],
-    'y_out': ['No', 'No', 'Yes', 'Yes', 'Yes', 'No',y1 'Noy_out'No']
+    'y3': [1, 0, 0, 2, 0, 2, 1, 2, 1, 0, 2, 2],
+    'y4': [0, 0, 0, 1, 2, 1, 2, 0, 1, 1, 2, 0],
+    'y_out': ['A', 'B', 'B', 'C', 'C', 'A', 'A', 'A', 'B', 'B', 'C', 'C']
 })
 
-information_gain = calculate_information_gain(data, 'Outlook', 'PlayTennis')
-print(f'Information Gain for Outlook: {information_gain}')
+for i in range(1, 5):
+    information_gain = calculate_information_gain(data, f'y{i}', 'y_out')
+    print(f'Information Gain for y{i}: {information_gain}')
